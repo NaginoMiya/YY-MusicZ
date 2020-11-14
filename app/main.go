62367784240f359beeb50ff59d2a22fedeb2f339
Noiri <suleipenn@gmail.com>
2020-11-14 15:50:53 +0900

    GetRandomMusicを実装しました。データはダミーです。

--- app/main.go
+++ app/main.go
@@ -4,10 +4,52 @@ import (
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"html/template"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"gopkg.in/olahol/melody.v1"
 )
+
+func GetRandomMusic(genre string) []string {
+	var ret []string
+	const number_of_songs = 5
+	const select_songs = 3
+	rand.Seed(time.Now().Unix())
+
+	var cnt []int
+	for i := 0; i < number_of_songs; i++ {
+		cnt = append(cnt, 0)
+	}
+
+	var randn []int
+
+	for len(randn) < select_songs {
+		x := rand.Intn(number_of_songs)
+		if cnt[x] == 0 {
+			cnt[x] = 1
+			randn = append(randn, x)
+		}
+	}
+
+
+	var Jpop = [...] string{"a", "b", "c", "d", "e"}
+	var Jazz = [...] string{"a", "b", "c", "d", "e"}
+
+	switch genre {
+	case "Jpop":
+		for i := 0; i < select_songs; i++ {
+			ret = append(ret, Jpop[randn[i]])
+		}
+	case "Jazz":
+		for i := 0; i < select_songs; i++ {
+			ret = append(ret, Jazz[randn[i]])
+		}
+	default:
+	}
+
+	return ret
+}
 
 func main(){
 	router := gin.Default()
@@ -33,6 +75,8 @@ func main(){
 
 	//チャット部分
 	router.GET("/music", func(c *gin.Context) {
+
+		GetRandomMusic("Jpop")
 
 		router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/music.tmpl")))
 
