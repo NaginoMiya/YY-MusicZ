796e56d698da7089538b259f2f03799db35f3253
zipnofastener <zipnofastener@gmail.com>
2020-11-15 14:42:26 +0900

    main.goにジャンルごとのHandleRequestの追加

--- app/main.go
+++ app/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"github.com/gin-contrib/static"
-	"github.com/gin-gonic/gin"
 	"html/template"
 	"math/rand"
 	"net/http"
 	"time"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
 
 	"gopkg.in/olahol/melody.v1"
 )
@@ -32,9 +33,8 @@ func GetRandomMusic(genre string) []string {
 		}
 	}
 
-
-	var Jpop = [...] string{"a", "b", "c", "d", "e"}
-	var Jazz = [...] string{"a", "b", "c", "d", "e"}
+	var Jpop = [...]string{"a", "b", "c", "d", "e"}
+	var Jazz = [...]string{"a", "b", "c", "d", "e"}
 
 	switch genre {
 	case "Jpop":
@@ -51,7 +51,7 @@ func GetRandomMusic(genre string) []string {
 	return ret
 }
 
-func main(){
+func main() {
 	router := gin.Default()
 	router.Use(static.Serve("/css", static.LocalFile("css", true)))
 	router.Use(static.Serve("/js", static.LocalFile("js", true)))
@@ -80,79 +80,109 @@ func main(){
 
 		router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/music.tmpl")))
 
-		c.HTML(http.StatusOK, "base", gin.H{
-		})
+		c.HTML(http.StatusOK, "base", gin.H{})
 	})
 
 	router.GET("/jpop", func(c *gin.Context) {
-        router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/jpop.tmpl")))
+		router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/jpop.tmpl")))
 
-        c.HTML(http.StatusOK, "base", gin.H{
-        })
+		c.HTML(http.StatusOK, "base", gin.H{})
 	})
-	
+
 	router.GET("/rock", func(c *gin.Context) {
-        router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/rock.tmpl")))
+		router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/rock.tmpl")))
 
-        c.HTML(http.StatusOK, "base", gin.H{
-        })
+		c.HTML(http.StatusOK, "base", gin.H{})
 	})
-	
+
 	router.GET("/edm", func(c *gin.Context) {
-        router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/edm.tmpl")))
+		router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/edm.tmpl")))
 
-        c.HTML(http.StatusOK, "base", gin.H{
-        })
+		c.HTML(http.StatusOK, "base", gin.H{})
 	})
-	
+
 	router.GET("/hiphop", func(c *gin.Context) {
-        router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/hiphop.tmpl")))
+		router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/hiphop.tmpl")))
 
-        c.HTML(http.StatusOK, "base", gin.H{
-        })
+		c.HTML(http.StatusOK, "base", gin.H{})
 	})
-	
+
 	router.GET("/classic", func(c *gin.Context) {
-        router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/classic.tmpl")))
+		router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/classic.tmpl")))
 
-        c.HTML(http.StatusOK, "base", gin.H{
-        })
+		c.HTML(http.StatusOK, "base", gin.H{})
 	})
-	
+
 	router.GET("/game", func(c *gin.Context) {
-        router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/game.tmpl")))
+		router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/game.tmpl")))
 
-        c.HTML(http.StatusOK, "base", gin.H{
-        })
+		c.HTML(http.StatusOK, "base", gin.H{})
 	})
-	
+
 	router.GET("/vocaloid", func(c *gin.Context) {
-        router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/vocaloid.tmpl")))
+		router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/vocaloid.tmpl")))
 
-        c.HTML(http.StatusOK, "base", gin.H{
-        })
+		c.HTML(http.StatusOK, "base", gin.H{})
 	})
-	
+
 	router.GET("/anime", func(c *gin.Context) {
-        router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/anime.tmpl")))
+		router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/anime.tmpl")))
 
-        c.HTML(http.StatusOK, "base", gin.H{
-        })
+		c.HTML(http.StatusOK, "base", gin.H{})
 	})
-	
-	router.GET("/all", func(c *gin.Context) {
-        router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/all.tmpl")))
 
-        c.HTML(http.StatusOK, "base", gin.H{
-        })
-    })
+	router.GET("/all", func(c *gin.Context) {
+		router.SetHTMLTemplate(template.Must(template.New("main").ParseFiles(layout, "template/all.tmpl")))
+
+		c.HTML(http.StatusOK, "base", gin.H{})
+	})
 
 	//melodyの実装部
-	router.GET("/ws", func(c *gin.Context){
+	router.GET("/ws", func(c *gin.Context) {
 		m.HandleRequest(c.Writer, c.Request)
 	})
 
-	m.HandleMessage(func(s *melody.Session, msg []byte){
+	router.GET("/jpop", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	router.GET("/rock", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	router.GET("/edm", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	router.GET("/hiphop", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	router.GET("/classic", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	router.GET("/game", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	router.GET("/game", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	router.GET("/vocaloid", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	router.GET("/anime", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	router.GET("/all", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
 		m.Broadcast(msg)
 	})
 
